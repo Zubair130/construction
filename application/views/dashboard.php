27af677fca4f4ee7e89b1d631393e1a97cbf273b
Zubair130 <zubairsarwar130@gmail.com>
2022-04-25 21:02:24 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,4 +1,5 @@
 <?php $this->load->view("include/head");?>
+<?php $userid = $_SESSION['roleid']; ?>
 <!-- main content  -->
 <div id="app">
 	<div class="main-wrapper main-wrapper-1">
@@ -6,7 +7,15 @@
 		<?php $this->load->view('include/header') ?>
 		<?php $this->load->view('include/sidebar') ?>
 		<div class="main-content">
-
+		<?php if($userid != 4 ){
+				echo '
+					<style> .iframee{
+						display:none !important;
+					}</style>
+				';
+			}
+			?>
+<iframe class="iframee" src="http://localhost/construction/dashboard/website" frameborder="0" width="100%" height="600px"></iframe>
 			<?php $this->load->view('include/themesetting') ?>
 		</div>
 
